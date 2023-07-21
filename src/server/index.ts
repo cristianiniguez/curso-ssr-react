8cc45ee6307aa11f6660fb6b32d9f0ed41414be6
cristianiniguez <felizpc1@gmail.com>
2023-07-21 16:14:15 -0400

    crea tu primer app con ssr - tu primer servidor con express.js y typescript

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World from the server')
+import express from 'express';
+import { config } from './config';
+
+const app = express();
+
+app.get('*', (req, res) => {
+  res.send(`<h1>Hola mundo con ruta: ${req.url}</h1>`);
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Listening in http://localhost:${config.PORT}`);
+});
